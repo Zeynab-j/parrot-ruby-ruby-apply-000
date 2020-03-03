329fa1c4d5ccc80dfc0ece3d4925f88064ee7fdc
Zeynab Jibreel <zeina2025@gmail.com>
2020-03-03 21:45:03 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot (phrase = parrot, phrase = squawk!)
+def parrot (phrase = squawk!)
 puts phrase
- phrase
+ phrase = parrot 
 end
